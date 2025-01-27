000ce3f14fb4db50d3cc7d6f62ffb7fb31f67ae1
hhhhhhjs <1512403978@qq.com>
2025-01-27 19:00:24 +0800

    feat:初始化配置

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className='text-red-200'>hhh</div>
+    </>
+  )
+}
+
+export default App
